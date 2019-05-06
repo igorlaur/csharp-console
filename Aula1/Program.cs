21eca86c353807fa0a19b1a7e116cabd7cc9462e
Igor Laur <igor.laur2@gmail.com>
2019-05-05 21:03:59 -0300

    Add project files.

---
+++ Aula1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Condicao;
+            Console.WriteLine("Hello World");
+            Console.WriteLine("How are you?");
+            Condicao = Console.ReadLine();
+
+            Console.WriteLine("A sua condição é: " + Condicao);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Um diretor de uma empresas quer saber se os seus funcionários estão gostando da empresa, 
+             porém caso alguém não goste ele quer saber o motivo. APENAS UM FUNCIONÁRIO.
+            */
+            string seNao;
+            string situacaoFuncionario;
+            Console.WriteLine("Você está gostando de trabalhar em nossa empresa? (Sim ou Não)");
+            situacaoFuncionario = Console.ReadLine();
+
+            if(situacaoFuncionario.ToLower() == "sim")
+            {
+                Console.WriteLine("Poxa, ficamos muito felizes em saber que você está gostando de trabalhar conosco!");
+            }
+            else if (situacaoFuncionario.ToLower() == "não")
+            {
+                Console.WriteLine("Poxa, como podemos melhorar?");
+                seNao = Console.ReadLine();
+                Console.WriteLine("A sua resposta é: " + seNao);
+            }
+            Console.ReadKey();
+        }
+    }
+}
